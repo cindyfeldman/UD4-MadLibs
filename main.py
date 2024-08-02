1f75e5988a91610ab0c9436933b2f90c3b6fc709
Cindy Feldman <30363621+cindyfeldman@users.noreply.github.com>
2024-08-01 22:15:42 -0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,29 @@
+'''
+DEVELOPER: <your name>
+COLLABORATORS: <anyone you worked with>
+DATE: <date last worked on>
+'''
+
+"""A one line summary of the program, terminated by a period.
+
+Leave one blank line.  The rest of this docstring should contain an
+overall description of the program.
+"""
+
+##########################################
+# IMPORTS:
+#   <list modules needed for program and their purpose>
+##########################################
+#<replace this line with import statement(s)>
+
+
+##########################################
+# FUNCTIONS:
+##########################################
+#<replace this line with function definitions, each needs a description>
+
+
+##########################################
+# MAIN PROGRAM:
+##########################################
+#<replace this line with your main program>
